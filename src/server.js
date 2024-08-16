d514ade44ee48e2068c2367871f415316e239a7e
Italo Chiaradia <italocoelho050704@gmail.com>
2024-08-16 08:23:03 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors");  
+require("dotenv/config");
+
 const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
@@ -30,5 +32,6 @@ app.use(( error, request, response, next ) => {
 
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
+
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
